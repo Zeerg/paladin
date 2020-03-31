a6a9780c101f33206472775387424f0b2dc8ac39
Tom <tbowyer@outlook.com>
2020-03-31 18:47:01 -0500

    start work on exfil tests

--- cmd/exfil.go
+++ cmd/exfil.go
@@ -9,8 +9,8 @@ import (
 // exfilCmd represents the exfil command
 var exfilCmd = &cobra.Command{
 	Use:   "exfil",
-	Short: "Run exfil like tests on current server",
-	Long: `Run exfil like tests on current server`,
+	Short: "Run exfil like tests on current host",
+	Long: `Run exfil like tests on current host`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("exfil called")
 	},
@@ -19,4 +19,6 @@ var exfilCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(exfilCmd)
 
+	exfilCmd.Flags().Bool("dns", false, "Runs DNS like exfil tests")
+	exfilCmd.Flags().Bool("ping", false, "Runs ping like exfil tests")
 }
